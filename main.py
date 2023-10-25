a4c3d89253540c554e4dfe49ad403142455c3f0a
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2023-10-25 09:16:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-from flask import Flask
 from menu import *
 
-main = Flask(__name__)
-@main.route("/")
-def runMenu():
-    menu = Menu()
-    start = True
+menu = Menu()
+start = True
 
-    print("Hello and welcome to FilmFlix:")
-    while(start):
-        menu.displayMain()
-        menu.getInput()
-        start=menu.getExit()
+print("Hello and welcome to FilmFlix:")
+while(start):
+    menu.displayMain()
+    menu.getInput()
+    start=menu.getExit()
 
-if __name__ == '__main__':
-    main.run()
 
